b30d51629118d58895c968e4d334b094f19329e4
Joanna Lin <lin.joanna.y@gmail.com>
2020-05-16 10:35:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = 34 % 10;
+
+let max = Math.max(1,2,3,20);
